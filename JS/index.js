b9ec7f1b2f0c3d6dc83052d1d906a7c06f5d031b
Andreen-Muchiri <andreenshii@gmail.com>
2022-09-01 11:57:26 +0300

    Interpolate to p tag

--- JS/index.js
+++ JS/index.js
@@ -19,12 +19,13 @@ btn.addEventListener("click",() =>{
   </div>
   <!-- Details-->
   <div class="details">
-    <p>pos</p>
+    <p>${data[0].meanings[0].partsOfSpeech}</p>
     <p>/sample/</p>
 </div> 
 <p class="word-meaning">
   A small part or quantity intended to show what the whole is like.
 </p>
-<p class="word-example">Investigations involved analysing samples of handwriting</p>`;
+<p class="word-example">Investigations involved analysing samples of handwriting
+</p>`;
 })
 });
