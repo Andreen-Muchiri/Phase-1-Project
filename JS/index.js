532b8696b483a9edf6f08223c9d254eff3cb01a9
Andreen-Muchiri <andreenshii@gmail.com>
2022-09-01 11:52:33 +0300

    Add result.innerHTML

--- JS/index.js
+++ JS/index.js
@@ -10,6 +10,21 @@ btn.addEventListener("click",() =>{
 .then((response)=>response.json())
 .then((data)=> {
     console.log(data);
-    result.innerHTML = ``;
+    result.innerHTML = `
+     <div class="word">
+    <h3>${inpWord}</h3>
+    <button>
+     <i class="fas fa-volume-up"></i>
+    </button>
+  </div>
+  <!-- Details-->
+  <div class="details">
+    <p>pos</p>
+    <p>/sample/</p>
+</div> 
+<p class="word-meaning">
+  A small part or quantity intended to show what the whole is like.
+</p>
+<p class="word-example">Investigations involved analysing samples of handwriting</p>`;
 })
 });
